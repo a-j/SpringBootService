16fcbd765d95814073230be5c6fe787793bdaee8
Anand Jayabalan <ajayabalan@sapient.com>
2016-11-26 13:28:04 -0500

    Initial commit

---
+++ src/main/java/aj/sample/hello/HelloController.java
@@ -0,0 +1,13 @@
+package aj.sample.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
